92c654a7d1407ae437aa42c18e9518fba581d083
Sergey Onufrienko <sergey@onufrienko.com>
2019-07-12 15:39:58 +0300

    Implement Record

--- src/record.js
+++ src/record.js
@@ -24,7 +24,6 @@
 **--------------------------------------------------------------------------*/
 
 import * as Utils from './utils.js';
-import * as Webm from './webm.js';
 import * as Messages from './spicemsg.js';
 import { Constants } from './enums.js';
 import { SpiceConn } from './spiceconn.js';
@@ -41,17 +40,102 @@ function SpiceRecordConn()
 SpiceRecordConn.prototype = Object.create(SpiceConn.prototype);
 SpiceRecordConn.prototype.process_channel_message = function(msg)
 {
+    if (!!!navigator.mediaDevices)
+    {
+        this.log_err('MediaDevices API is not available');
+        return false;
+    }
+
     if (msg.type == Constants.SPICE_MSG_RECORD_START)
     {
+        var startData = new Messages.SpiceMsgRecordStart(msg.data);
         Utils.DEBUG > 0 && console.log("RECORD_START");
+        Utils.DEBUG > 0 && console.log(startData);
+
+        this.send_start_mark_message();
+        this.send_mode_message();
+        this.start_microphone();
     }
 
     if (msg.type == Constants.SPICE_MSG_RECORD_STOP)
     {
         Utils.DEBUG > 0 && console.log("RECORD_STOP");
+        debugger;
     }
 
     return false;
+}
+
+SpiceRecordConn.prototype.send_start_mark_message = function() {
+    var startTimestamp = Date.now();
+    var data = new Messages.SpiceMsgcRecordStartMark(startTimestamp);
+    var msg = new Messages.SpiceMiniData();
+    msg.build_msg(Constants.SPICE_MSGC_RECORD_START_MARK, data);
+    this.send_msg(msg);
+}
+
+SpiceRecordConn.prototype.send_mode_message = function() {
+    var data = new Messages.SpiceMsgcRecordMode(Constants.SPICE_RECORD_DATA_MODE_RAW);
+    var msg = new Messages.SpiceMiniData();
+    msg.build_msg(Constants.SPICE_MSGC_RECORD_MODE, data);
+    this.send_msg(msg);
+}
+
+SpiceRecordConn.prototype.send_data_message = function(dataInUint8Array) {
+    var data = new Messages.SpiceMsgcRecordData(new Date(), dataInUint8Array);
+    var msg = new Messages.SpiceMiniData();
+    msg.build_msg(Constants.SPICE_MSGC_RECORD_DATA, data);
+    this.send_msg(msg);
+}
+
+SpiceRecordConn.prototype.start_microphone = function() {        
+    var constraints = { 
+        audio: { 
+            echoCancellation: true
+        }
+    };
+
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(this.use_microphone_stream.bind(this))
+        .catch(this.log_err);
+}
+
+SpiceRecordConn.prototype.use_microphone_stream = function(stream) {
+    // Cross browser
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
+    var audioContext = new AudioContext();
+
+    var bufferSize = 2048;
+
+    var gain_node = audioContext.createGain();
+    gain_node.connect(audioContext.destination);
+
+    var microphone_stream = audioContext.createMediaStreamSource(stream);
+    microphone_stream.connect(gain_node);
+
+    var script_processor_node = audioContext.createScriptProcessor(bufferSize, 1, 1);
+    microphone_stream.connect(script_processor_node);
+
+    var script_processor_fft_node = audioContext.createScriptProcessor(bufferSize, 1, 1);
+    script_processor_fft_node.connect(gain_node);
+
+    var analyserNode = audioContext.createAnalyser();
+    analyserNode.smoothingTimeConstant = 0;
+    analyserNode.fftSize = bufferSize;
+
+    microphone_stream.connect(analyserNode);
+    analyserNode.connect(script_processor_fft_node);
+
+    script_processor_fft_node.onaudioprocess = this.microphone_onaudioprocess.bind(this, analyserNode);
+}
+
+SpiceRecordConn.prototype.microphone_onaudioprocess = function(analyserNode) {
+    var array = new Uint8Array(analyserNode.frequencyBinCount);
+        
+    // copies the current frequency data into a Uint8Array
+    analyserNode.getByteFrequencyData(array);
+    this.send_data_message(array);
 }
 
 export {
